ae9dd7415ea57bb9e5e31de54e0b6dd89f4759f5
kimzhek <kimzhek@icloud.com>
2022-04-25 14:04:18 +0900

    새로운 시작

--- README.md
+++ README.md
@@ -1,12 +1,30 @@
-
-
 node.js 설치
+
 npm 설치
+
 git 설치
 
 npm init
+
 npm install
+
 gulpfile.js 복사
 
+npm install gulp -g 
 
 npm install gulp gulp-concat gulp-file-include gulp-uglify gulp-rename gulp-sass gulp-sourcemaps gulp-clean gulp-autoprefixer del path browser-sync sass --save-dev
+
+
+git clone  주소
+
+git remote add origin 깃주소
+
+git status
+
+git add .
+
+git commit -m"설명"
+
+git push
+
+git stash pop
--- gulpfile.js
+++ gulpfile.js
@@ -1,45 +1,3 @@
-// var gulp = require('gulp');
-// var scss = require('gulp-sass');
-// var sourcemaps = require('gulp-sourcemaps');
-
-
-// var PATH = {
-//     ASSETS: {
-//         FONTS: './src/fonts',
-//         IMAGES: './src/images',
-//         STYLE: './src/css'
-//     }
-// },
-// DEST_PATH = {
-//     ASSETS: {
-//         FONTS: './dist/fonts',
-//         IMAGES: './dist/images',
-//         STYLE: './dist/css'
-//     }
-// };
-// gulp.task('scss:compile', () => {
-//     return new Promise(resolve => {
-//         var options = {
-//             outputStyle: "nested",
-//             indentType: "space",
-//             indentWidth: 4,
-//             precision: 8,
-//             sourceComments: true
-//         };
-//         gulp.src(PATH.ASSETS.STYLE + '/*.scss')
-//         .pipe(sourcemaps.init())
-//         .pipe(scss(options))
-//         .pipe(sourcemaps.write())
-//         .pipe(gulp.dest(DEST_PATH.ASSETS.STYLE));
-        
-//         resolve();
-//     });
-// });
-
-// gulp.task('default', gulp.series(['scss:compile']));
-
-
-
 var gulp = require('gulp');
 
 var concat = require('gulp-concat'),
@@ -60,7 +18,8 @@ var paths = {
     html : src + '/**/*.html',
     js : src + '/js/**/*.js',
     scss : src + '/scss/**/*.scss',
-    img : src + '/images/**/*.*'
+    img: src + '/images/**/*.*',
+    fonts: src + '/fonts/**/*.*'
 };
 
 // clean
@@ -86,7 +45,7 @@ gulp.task('html', function () {
 
 // scss option
 var scssOptions = {
-    outputStyle: "compact",
+    // outputStyle: "compressed",
     indentType: "tab",
     indentWidth: 1,
     precision: 6,
@@ -97,11 +56,11 @@ var scssOptions = {
 gulp.task('scss:compile', function () {
     return gulp
         .src(paths.scss) //불러오기
-        .pipe(sourcemaps.init())//소스맵 초기화
+        // .pipe(sourcemaps.init())//소스맵 초기화
         .pipe(scss(scssOptions).on('error', scss.logError))
-        .pipe(concat('common.css')) //병합
+        .pipe(concat('style.css')) //병합
         .pipe(autoprefixer())
-        .pipe(sourcemaps.write()) //소스맵
+        // .pipe(sourcemaps.write()) //소스맵
         .pipe(gulp.dest(dist + '/css')) //생성
         .pipe(browserSync.stream());
 });
@@ -111,12 +70,12 @@ gulp.task('js:combine', function () {
     return gulp
         .src(paths.js) //불러오기
         // .pipe(sourcemaps.init())//소스맵 초기화
-        .pipe(concat('common.js')) // 병합
-        .pipe(gulp.dest('./dist/js')) // 생성
-        .pipe(uglify()) //난독화
-        .pipe(rename('common.min.js')) //이름바꾸기
+        // .pipe(concat('common.js')) // 병합
+        // .pipe(gulp.dest('./dist/js')) // 생성
+        // .pipe(uglify()) //난독화
+        // .pipe(rename('common.min.js')) //이름바꾸기
         // .pipe(sourcemaps.write()) //소스맵
-        .pipe(gulp.dest('./dist/js')) // 생성
+        .pipe(gulp.dest(dist + '/js')) // 생성
         .pipe(browserSync.stream());
 
 });
@@ -130,42 +89,35 @@ gulp.task('img', function () {
 
 });
 
+// fonts
+gulp.task('fonts', function () {
+    return gulp
+        .src(paths.fonts) //불러오기
+        .pipe(gulp.dest('./dist/fonts')) // 생성
+        .pipe(browserSync.stream());
+
+});
+
 // browserSync
 gulp.task('browserSync', () => {
     return new Promise(resolve => {
         browserSync.init(null, {
-            // proxy: 'http://localhost:8888',
             port: 9999,
-            server: "./dist"
+            server: {
+                baseDir: 'dist',
+                index: "status.html"
+            },
+            browser: "chrome"
         });
         resolve();
     });
 });
 
 function watch(){
-
-// gulp.task('watch', function () {
-
-    // browserSync.init({
-    //     port: 3333,
-    //     server: "./dist"
-    // });
-
     gulp.watch(paths.html, gulp.series('html'));
     gulp.watch(paths.js, gulp.series('js:combine'));
     gulp.watch(paths.scss, gulp.series('scss:compile'));
     gulp.watch(paths.img, gulp.series('img'));
+};
 
-
-    // var watcher = gulp.watch(paths.html, gulp.series('html'));
-    // var watcher = gulp.watch(paths.img, gulp.series('img'));
-    // watcher.on('unlink', function (filepath) {
-    //     var destFilePath = path.basename(filepath);
-    //     del.sync(dist + '/**/' + destFilePath);
-    // });
-// });
-
-}
-
-// gulp.task('default', gulp.series('browserSync', 'watch'));
-gulp.task('default', gulp.series('clean', 'html', 'scss:compile', 'js:combine', 'img', 'browserSync', watch));
+gulp.task('default', gulp.series('clean', 'html', 'scss:compile', 'js:combine', 'img', 'fonts', 'browserSync', watch));
